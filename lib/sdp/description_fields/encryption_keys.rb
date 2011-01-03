f7cdde98e987ec82cb9b373fcc0a7ce674c84b2f
Steve Loveless <steve.loveless@gmail.com>
2011-01-02 23:56:58 -0800

    Added encryption_keys field

---
+++ lib/sdp/description_fields/encryption_keys.rb
@@ -0,0 +1,38 @@
+class SDP::DescriptionFields
+  class EncryptionKeysField < SDP::DescriptionField
+    def initialize value=nil
+      @sdp_type = 'k'
+      @ruby_type = :encryption_keys
+      @required = false
+      @value = {
+        :method => "",    # CT or AS
+        :encryption_key => nil
+      }
+
+      unless value.nil?
+        super
+        map_values
+      end
+    end
+
+    # If a values string was passed in, the parent class broken those up
+    # to an Array.  This maps those values to the value Hash.
+    def map_values
+      @value[:method]         = @parsed_values[0]
+      @value[:encryption_key] = @parsed_values[1]
+    end
+
+    # Redefining parent method in order to inject a ":" between the two
+    # required fields.  A resulting encryption keys string should look
+    # like: "k=clear:mypassword".
+    # 
+    # @return [String] A String in the form: "v=0\r\n".
+    def to_sdp_s
+      unless @value[:method].empty? || @value[:encryption_key].nil?
+        values = "#{@value[:method]}:#{@value[:encryption_key]}"
+      end
+
+      return "#{@sdp_type}=#{values}\r\n"
+    end
+  end
+end
