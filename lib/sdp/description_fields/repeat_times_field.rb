3bbc4005f80862d5ef40faa4c7eaee75f7a535bd
Steve Loveless <steve.loveless@gmail.com>
2011-01-03 16:24:43 -0800

    Added unit tests for repeat times field

--- lib/sdp/description_fields/repeat_times_field.rb
+++ lib/sdp/description_fields/repeat_times_field.rb
@@ -18,6 +18,22 @@ class SDP::DescriptionFields
       end
     end
 
+    # Redefines value assingment to allow for changing parameters separately.
+    #
+    # @param [Hash] new_value Key must be an existing @value key or pair.
+    def value=(new_value)
+      if new_value[:repeat_interval] && new_value[:active_duration] &&
+        new_value[:offsets_from_start_time]
+        @value = new_value
+      elsif new_value[:repeat_interval]
+        @value[:repeat_interval] = new_value[:repeat_interval]
+      elsif new_value[:active_duration]
+        @value[:active_duration] = new_value[:active_duration]
+      elsif new_value[:offsets_from_start_time]
+        @value[:offsets_from_start_time] = new_value[:offsets_from_start_time]
+      end
+    end
+
     def map_values
       @value[:repeat_interval]          = @parsed_values[0]
       @value[:active_duration]          = @parsed_values[1]
