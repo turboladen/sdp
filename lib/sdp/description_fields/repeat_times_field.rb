5fb0d765d9ee1c35887c64a101fbf4f7cba518b5
Steve Loveless <steve.loveless@gmail.com>
2011-01-03 18:04:53 -0800

    Refactored value= method

--- lib/sdp/description_fields/repeat_times_field.rb
+++ lib/sdp/description_fields/repeat_times_field.rb
@@ -22,15 +22,10 @@ class SDP::DescriptionFields
     #
     # @param [Hash] new_value Key must be an existing @value key or pair.
     def value=(new_value)
-      if new_value[:repeat_interval] && new_value[:active_duration] &&
-        new_value[:offsets_from_start_time]
-        @value = new_value
-      elsif new_value[:repeat_interval]
-        @value[:repeat_interval] = new_value[:repeat_interval]
-      elsif new_value[:active_duration]
-        @value[:active_duration] = new_value[:active_duration]
-      elsif new_value[:offsets_from_start_time]
-        @value[:offsets_from_start_time] = new_value[:offsets_from_start_time]
+      @value.each_pair do |k,v|
+        if new_value.has_key?(k)
+          @value[k] = new_value[k]
+        end
       end
     end
 
