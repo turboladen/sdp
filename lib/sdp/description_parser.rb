cafe205d8e345393a8435a58fb04b067f8078156
Steve Loveless <steve.loveless@gmail.com>
2011-01-13 00:19:02 -0800

    So far so good with parslet... still more to go

--- lib/sdp/description_parser.rb
+++ lib/sdp/description_parser.rb
@@ -18,31 +18,44 @@ class SDPDescription < Parslet::Parser
 
   root :description
 =end
-  rule(:version_name)   { str('v=') }
-  rule(:version)        { version_name >> field_value.as(:protocol_version) }
+  rule(:version)        { str('v=') >> field_value.as(:protocol_version) >> eol }
 
-  rule(:origin_name)    { str('o=') }
   rule(:origin) do
-    origin_name >> field_value.as(:username) >> space >> field_value.as(:session_id) >> space >>
+    str('o=') >> field_value.as(:username) >> space >> field_value.as(:session_id) >> space >>
     field_value.as(:session_version) >> space >> field_value.as(:network_type) >> space >>
-    field_value.as(:address_type) >> space >> field_value.as(:unicast_address)
+    field_value.as(:address_type) >> space >> field_value.as(:unicast_address) >> eol
   end
 
-  rule(:session_name_name)   { str('s=') }
-  rule(:session_name)   { session_name_name >> match('[^\n]').repeat.as(:session_name)}
+  rule(:session_name)        { str('s=') >> field_value_string.as(:session_name) >> eol }
+  rule(:session_information) { str('i=') >> field_value_string.as(:session_information) >> eol }
+  rule(:uri)            { str('u=') >> field_value.as(:uri) >> eol }
+  rule(:email_address)  { str('e=') >> field_value_string.as(:email_address) >> eol }
+  rule(:phone_number)   { str('p=') >> field_value_string.as(:phone_number) >> eol }
+  rule(:connection_address) do
+    str('c=') >> field_value >> space >> field_value >> space >>
+    field_value.as(:connection_address) >> eol
+  end
+
   # Generics
   rule(:space)          { match('[ ]').repeat(1) }
   rule(:eol)            { match('[\n]') }
   rule(:field_value)    { match('\S').repeat }
+  rule(:field_value_string) { match('[^\n]').repeat }
 
-  rule(:description)    { version >> eol >> origin >> eol >> session_name >> eol }
+  rule(:description) do
+    version >> origin >> session_name >> 
+    (session_information.maybe >> uri.maybe >> email_address.maybe >> phone_number.maybe >> connection_address.maybe)
+  end
+
   root :description
 end
 
 s = SDPDescription.new
-#p s.parse "v=1"
-#p s.parse "v=1\n"
-#p s.parse "v=1\no=steve\n"
+p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\ni=And here's some info\nu=http://bobo.net/thispdf.pdf\ne=bob@thing.com (Bob!)\np=+1 555 123 0987\nc=IN IP4 224.5.234.22/24\n"
+p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\ni=And here's some info\nu=http://bobo.net/thispdf.pdf\ne=bob@thing.com (Bob!)\np=+1 555 123 0987\n"
+p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\ni=And here's some info\nu=http://bobo.net/thispdf.pdf\ne=bob@thing.com (Bob!)\n"
+p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\ni=And here's some info\nu=http://bobo.net/thispdf.pdf\n"
+p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\ni=And here's some info\n"
 p s.parse "v=1\no=steve 1234 5555 IN IP4 123.33.22.123\ns=This is a test session\n"
 
 =begin
