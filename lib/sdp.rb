e61276905ccc09fac9b615f06d4f9ba39d46b8e5
Steve Loveless <steve.loveless@gmail.com>
2011-01-18 08:43:30 -0800

    Moved description_parser to parser

--- lib/sdp.rb
+++ lib/sdp.rb
@@ -1,10 +1,14 @@
 require 'sdp/version'
+require 'sdp/description'
 require 'sdp/parser'
 
 class SDP
-  include SDP::Parser
-
   SDP_VERSION = 0
+  
+  def self.parse sdp_text
+		sdp_hash = SDP::DescriptionParser.new.parse sdp_text
+    session = SDP::Description.new(sdp_hash)
+  end
 end
 
 class SDP::RuntimeError < StandardError; end;
