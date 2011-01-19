ef7b03f56e656b6752da5e47a2bc7fce8b5c7b42
Steve Loveless <steve.loveless@gmail.com>
2011-01-18 19:14:20 -0800

    Reordered tests to follow spec (easier to read) and added more tests for complex field types

--- spec/sdp/description_spec.rb
+++ spec/sdp/description_spec.rb
@@ -7,64 +7,13 @@ describe SDP::Description do
   end
 
   it "initializes with the protoco_version value set" do
-    @sdp.protocol_version.should == 0
+    @sdp.protocol_version.should == SDP::PROTOCOL_VERSION
   end
 
   context "can add and retrieve fields" do
-    it "attribute" do
-      new_values = { :attribute => 'rtpmap',
-        :value => "99 h263-1998/90000" }
-      @sdp.attributes << new_values
-      @sdp.attributes.first.should == new_values
-    end
-
-    it "bandwidth_type" do
-      @sdp.bandwidth = :CT
-      @sdp.bandwidth.should == :CT
-    end
-
-    it "bandwidth" do
-      @sdp.bandwidth = 100
-      @sdp.bandwidth.should == 100
-    end
-
-    it "network_type" do
-      @sdp.network_type = :IN
-      @sdp.network_type.should == :IN
-    end
-
-    it "address_type" do
-      @sdp.address_type = :IP4
-      @sdp.address_type.should == :IP4
-    end
-
-    it "connection_address" do
-      @sdp.address_type = 'localhost'
-      @sdp.address_type.should == 'localhost'
-    end
-
-    it "email_address" do
-      @sdp.email_address = 'me@me.com'
-      @sdp.email_address.should == 'me@me.com'
-    end
-  
-    it "encryption_method" do
-      @sdp.encryption_method = 'clear'
-      @sdp.encryption_method.should == 'clear'
-    end
-  
-    it "encryption_key" do
-      @sdp.encryption_key = 'password'
-      @sdp.encryption_key.should == 'password'
-    end
-  
-    it ":media_sections" do
-      new_values = { :media => 'audio',
-        :port => 12345,
-        :protocol => 'RTP/AVP',
-        :format => 99 }
-      @sdp.media_sections << new_values
-      @sdp.media_sections.first.should == new_values
+    it "protocol_version" do
+      @sdp.protocol_version = 1
+      @sdp.protocol_version.should == 1
     end
 
     it "username" do
@@ -82,14 +31,69 @@ describe SDP::Description do
       @sdp.version.should == 2890842807
     end
 
+    it "network_type" do
+      @sdp.network_type = :IN
+      @sdp.network_type.should == :IN
+    end
+
+    it "address_type" do
+      @sdp.address_type = :IP4
+      @sdp.address_type.should == :IP4
+    end
+
     it "unicast_address" do
       @sdp.unicast_address = "10.47.16.5"
       @sdp.unicast_address.should == "10.47.16.5"
     end
 
+    it "name" do
+      @sdp.name = "This is a session"
+      @sdp.name.should == "This is a session"
+    end
+
+    it "information" do
+      @sdp.information = "This is a session"
+      @sdp.information.should == "This is a session"
+    end
+
+    it "uri" do
+      @sdp.uri = "http://localhost"
+      @sdp.uri.should == "http://localhost"
+    end
+
+    it "email_address" do
+      @sdp.email_address = 'me@me.com'
+      @sdp.email_address.should == 'me@me.com'
+    end
+
     it "phone_number" do
       @sdp.phone_number = "+1 555 123 4567"
       @sdp.phone_number.should == "+1 555 123 4567"
+    end
+
+    it "connection_address" do
+      @sdp.address_type = 'localhost'
+      @sdp.address_type.should == 'localhost'
+    end
+
+    it "bandwidth_type" do
+      @sdp.bandwidth = :CT
+      @sdp.bandwidth.should == :CT
+    end
+
+    it "bandwidth" do
+      @sdp.bandwidth = 100
+      @sdp.bandwidth.should == 100
+    end
+
+    it "start_time" do
+      @sdp.start_time = 99112299
+      @sdp.start_time.should == 99112299
+    end
+
+    it "stop_time" do
+      @sdp.stop_time = 99999999
+      @sdp.stop_time.should == 99999999
     end
   
     it "repeat_interval" do
@@ -106,18 +110,8 @@ describe SDP::Description do
       @sdp.offsets_from_start_time = 99
       @sdp.offsets_from_start_time.should == 99
     end
-
-    it "session_information" do
-      @sdp.information = "This is a session"
-      @sdp.information.should == "This is a session"
-    end
-
-    it "session_name" do
-      @sdp.name = "This is a session"
-      @sdp.name.should == "This is a session"
-    end
   
-    context ":time_zones" do
+    context "time_zones" do
       it "one time zone" do
         new_values = { :time_zone_adjustment => 111111,
           :time_zone_offset => 99 }
@@ -135,55 +129,92 @@ describe SDP::Description do
         @sdp.time_zones.should == [new_values1, new_values2]
       end
     end
-
-    it "start_time" do
-      @sdp.start_time = 99112299
-      @sdp.start_time.should == 99112299
+  
+    it "encryption_method" do
+      @sdp.encryption_method = 'clear'
+      @sdp.encryption_method.should == 'clear'
+    end
+  
+    it "encryption_key" do
+      @sdp.encryption_key = 'password'
+      @sdp.encryption_key.should == 'password'
     end
 
-    it "stop_time" do
-      @sdp.stop_time = 99999999
-      @sdp.stop_time.should == 99999999
+    context "attributes" do
+      it "one attribute with value" do
+        new_values = { :attribute => 'rtpmap',
+          :value => "99 h263-1998/90000" }
+        @sdp.attributes << new_values
+        @sdp.attributes.first.should == new_values
+      end
+
+      it "one attribute with empty value" do
+        new_values = { :attribute => 'rtpmap',
+          :value => "" }
+        @sdp.attributes << new_values
+        @sdp.attributes.first.should == new_values
+      end
+
+      it "one attribute with nil value" do
+        new_values = { :attribute => 'rtpmap' }
+        @sdp.attributes << new_values
+        @sdp.attributes.first.should == new_values
+      end
+
+      it "two attributes" do
+        new_values1 = { :attribute => 'test' }
+        new_values2 = { :attribute => 'rtpmap',
+          :value => "99 h263-1998/90000" }
+        @sdp.attributes << new_values1
+        @sdp.attributes << new_values2
+        @sdp.attributes.should == [new_values1, new_values2]
+      end
     end
+  
+    context ":media_sections" do
+      it "can add a basic media section" do
+        new_values = { :media => 'audio',
+          :port => 12345,
+          :protocol => 'RTP/AVP',
+          :format => 99 }
+        @sdp.media_sections << new_values
+        @sdp.media_sections.first.should == new_values
+      end
 
-    it "uri" do
-      @sdp.uri = "http://localhost"
-      @sdp.uri.should == "http://localhost"
-    end
+      it "can add a basic media section with attributes" do
+        new_values = { :media => 'audio',
+          :port => 12345,
+          :protocol => 'RTP/AVP',
+          :format => 99,
+          :attributes => [
+            {
+              :attribute => "rtpmap",
+              :value => "99 h263-1998/90000"
+            }
+          ]
+        }
+        @sdp.media_sections << new_values
+        @sdp.media_sections.first.should == new_values
+      end
 
-    it "protocol_version" do
-      @sdp.protocol_version = 0
-      @sdp.protocol_version.should == 0
-    end
-  end
+      it "can add 2 basic media sections" do
+        new_values = []
+        new_values << { :media => 'audio',
+          :port => 12345,
+          :protocol => 'RTP/AVP',
+          :format => 99 }
+        @sdp.media_sections << new_values[0]
 
-  context "can add and retrieve > 1 field of same type" do
-    it ":attribute" do
-      @sdp.attributes << { :attribute => 'recvonly', :value => "" }
-      @sdp.attributes << { :attribute => 'rtpmap', :value => '99 h263-1998/90000' }
+        new_values << { :media => 'video',
+          :port => 5678,
+          :protocol => 'RTP/AVP',
+          :format => 33 }
+        @sdp.media_sections << new_values[1]
 
-      @sdp.attributes.class.should == Array
-      @sdp.attributes[0].should == { :attribute => 'recvonly', :value => "" }
-      @sdp.attributes[1].should == { :attribute => 'rtpmap', :value => '99 h263-1998/90000' }
-    end
-
-    it ":media_sections" do
-      new_values = []
-      new_values << { :media => 'audio',
-        :port => 12345,
-        :protocol => 'RTP/AVP',
-        :format => 99 }
-      @sdp.media_sections << new_values[0]
-
-      new_values << { :media => 'video',
-        :port => 5678,
-        :protocol => 'RTP/AVP',
-        :format => 33 }
-      @sdp.media_sections << new_values[1]
-
-      @sdp.media_sections.class.should == Array
-      @sdp.media_sections[0].should == new_values[0]
-      @sdp.media_sections[1].should == new_values[1]
+        @sdp.media_sections.class.should == Array
+        @sdp.media_sections[0].should == new_values[0]
+        @sdp.media_sections[1].should == new_values[1]
+      end
     end
   end
 end
