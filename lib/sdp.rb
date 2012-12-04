5b3cb96fae5bf6b440cbf8e25e00504217b83b7d
Steve Loveless <steve.loveless@gmail.com>
2012-12-04 15:19:56 -0800

    Parsed Descriptions now use Strings & Ints instead of Parslet::Slices

--- lib/sdp.rb
+++ lib/sdp.rb
@@ -1,16 +1,15 @@
-require File.expand_path(File.dirname(__FILE__) + '/sdp/version')
-require File.expand_path(File.dirname(__FILE__) + '/sdp/description')
-require File.expand_path(File.dirname(__FILE__) + '/sdp/parser')
-require File.expand_path(File.dirname(__FILE__) + '/sdp/parse_error')
-require File.expand_path(File.dirname(__FILE__) + '/ext/hash_patch')
+require_relative 'sdp/version'
+require_relative 'sdp/description'
+require_relative 'sdp/parser'
+require_relative 'sdp/parser_error'
+
 
 # The only use for this class is the +#parse+ method, which is in this
 # base class solely for convenience.  Other than this method, this
 # base class doesn't really do anything.
 class SDP
 
-  # Creates a parser and parses the given text in to an +SDP::Description+
-  # object.
+  # Parses the given text in to an +SDP::Description+ object.
   #
   # @param [String] sdp_text The text from an SDP description.
   # @return [SDP::Description] The object that represents the description
@@ -24,6 +23,27 @@ class SDP
       raise SDP::ParseError, ex
     end
 
-    SDP::Description.new(sdp_hash.keys_to_s)
+    SDP::Description.new(remove_parslet(sdp_hash))
+  end
+
+  private
+
+  # The Parser parses values as Parslet::Slice objects.  This converts those
+  # values to Strings and Integers.
+  #
+  # @param [Hash] sdp_hash The parsed Hash.
+  # @return [Hash] The converted Hash.
+  def self.remove_parslet(sdp_hash)
+    clean_hash = {}
+
+    sdp_hash.each do |section, values|
+      clean_hash[section] = values.inject({}) do |result, (k, v)|
+        string_value = v.str
+        result[k] = Integer(string_value) rescue string_value
+        result
+      end
+    end
+
+    clean_hash
   end
 end
