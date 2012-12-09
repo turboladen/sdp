0e1594dd93ead7c81bb1d1a42a31f2bbc60bcccb
Steve Loveless <steve.loveless@gmail.com>
2012-12-08 16:23:30 -0800

    Start of spec here

--- spec/integration/builds_descriptions_spec.rb
+++ spec/integration/builds_descriptions_spec.rb
@@ -37,4 +37,64 @@ describe "Create descriptions" do
     session.to_s.should == example_file
     session.should be_valid
   end
+
+  SDP::Logger.log = true
+
+  context "from scratch, adding fields in reverse order, no media sections" do
+    it "from scratch" do
+      d = SDP::Description.new
+      d.add_group :session_description
+      d.should_not be_valid
+
+      expect {
+        d.add_group :session_description
+      }.to raise_error SDP::RuntimeError
+
+      d.session_description.add_field :attribute
+      d.session_description.attributes.last.attribute = 'tool'
+      d.session_description.attributes.last.value = 'Test Tool v1'
+      d.session_description.add_field :attribute
+      d.session_description.add_field :attribute
+      d.session_description.add_field :encryption_key
+      d.session_description.add_field :time_zone_adjustments
+      d.session_description.add_field :bandwidth
+      d.session_description.add_field :bandwidth
+      d.session_description.add_field :bandwidth
+      d.session_description.add_field :connection_data
+      d.session_description.add_field :phone_number
+      d.session_description.add_field :email_address
+      d.session_description.add_field :uri
+      d.session_description.add_field :session_information
+      d.session_description.add_field :session_name
+      d.session_description.add_field :origin
+      d.session_description.add_field :protocol_version
+
+      d.session_description.add_group :time_description
+      d.session_description.time_descriptions.last.add_field :repeat_times
+      d.session_description.time_descriptions.last.add_field :timing
+
+      d.should_not be_valid
+      d.to_s.each_line { |l| p l }
+=begin
+      expect {
+        d.session_description.add_field :origin
+      }.to raise_error SDP::RuntimeError
+      d.session_description.add_field :origin
+      d.should_not be_valid
+
+      expect {
+        d.session_description.add_field :origin
+      }.to raise_error SDP::RuntimeError
+
+      d.session_description.add_field :protocol_version
+      d.should_not be_valid
+
+      expect {
+        d.session_description.add_field :protocol_version
+      }.to raise_error SDP::RuntimeError
+
+      p d
+=end
+    end
+  end
 end
