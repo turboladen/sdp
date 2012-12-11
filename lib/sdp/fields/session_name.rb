a1f7e7b7531afd1377f82b0638efd0a7232f118e
Steve Loveless <steve.loveless@gmail.com>
2012-12-11 10:14:26 -0800

    Changed value to 'name'

--- lib/sdp/fields/session_name.rb
+++ lib/sdp/fields/session_name.rb
@@ -4,7 +4,7 @@ require_relative '../field'
 class SDP
   module Fields
     class SessionName < SDP::Field
-      field_value :session_name
+      field_value :name
       prefix :s
 
       def initialize(init_data=nil)
@@ -14,14 +14,14 @@ class SDP
       def to_s
         super
 
-        "#{prefix}=#{@session_name}\r\n"
+        "#{prefix}=#{@name}\r\n"
       end
 
       private
 
       def add_from_string(init_data)
         match = init_data.match(/#{prefix}=(?<name>[^\r\n]+)/)
-        @session_name = match[:name]
+        @name = match[:name]
       rescue NoMethodError
         raise SDP::ParseError, "Error parsing string '#{init_data}'"
       end
